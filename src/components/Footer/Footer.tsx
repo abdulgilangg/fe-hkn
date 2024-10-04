c9351dae2504ca3edab20e67e605e7d22e65f602
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-10-04 23:25:25 +0700

    [ + ] components/ Footer.tsx

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="flex w-full p-5 bg-white drop-shadow-lg dark:bg-boxdark">
+      <strong className="text-gray-800 dark:text-white">
+        Copyright © 2024{' '}
+        <a
+          href="https://www.sumapala.co.id"
+          className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 transition-colors duration-200"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sumapala Integrasi Solusi.
+        </a>{' '}
+        All rights reserved.
+      </strong>
+    </footer>
+  );
+};
+
+export default Footer;
