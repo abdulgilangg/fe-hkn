c0bfd9da9727a0eff8b5ab5665e32420b8cd499c
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-10-06 16:21:46 +0700

    [ ^ ] components/ Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex w-full p-3 bg-white drop-shadow-lg dark:bg-boxdark">
-      <strong className="text-gray-500 dark:text-white">
+    <footer className="flex w-full p-3 bg-white drop-shadow-lg dark:bg-boxdark mt-auto">
+      <strong className="text-gray-500 dark:text-white mx-auto">
         Copyright © 2024{' '}
         <a
           href="https://www.sumapala.co.id"
-          className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 transition-colors duration-200"
+          className="text-primary hover:text-primary dark:text-blue-400 dark:hover:text-blue-300 transition-colors duration-200"
           target="_blank"
           rel="noopener noreferrer"
         >
