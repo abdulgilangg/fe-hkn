5d7e68f3a46e3ef0b64a365780a546ac45450020
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-10-06 20:41:43 +0700

    [ ^ ] components/ DealerComponents Settings.tsx

--- src/components/DealerComponents/Settings.tsx
+++ src/components/DealerComponents/Settings.tsx
@@ -138,6 +138,22 @@ const Settings = () => {
                     />
                   </div>
 
+                  <div className="mb-5.5">
+                    <label
+                      className="mb-3 block text-sm font-medium text-black dark:text-white"
+                      htmlFor="Address"
+                    >
+                      Address
+                    </label>
+                    <input
+                      className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                      type="text"
+                      name="Address"
+                      id="Address"
+                      placeholder="aleaa"
+                    />
+                  </div>
+
                   <div className="flex justify-end gap-4.5">
                     <button
                       className="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
