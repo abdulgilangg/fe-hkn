afb5879259016f2c3acb721696d3dde1a94fe4da
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-10-06 17:57:35 +0700

    [ ^ ] components/ Announcement.tsx, Breadcrumb.tsx

--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -27,29 +27,31 @@ const AnnouncementSlider: React.FC = () => {
   }, [currentIndex]);
 
   return (
-    <div className="relative w-full py-2 md:px-6 overflow-hidden text-white bg-primary bg-opacity-90 rounded-sm border shadow-default border-stroke mb-5 p-5 flex justify-between dark:border-strokedark dark:bg-boxdark dark:text-white">
-      <div
-        className={`flex transition-transform duration-500 ease-in-out ${
-          !isTransitioning && 'transition-none'
-        }`}
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        onTransitionEnd={() => {
-          if (currentIndex === announcements.length) {
-            setIsTransitioning(false);
-            setCurrentIndex(0);
-          }
-        }}
-      >
-        {announcements.concat(announcements[0]).map((announcement, index) => (
-          <div
-            key={index}
-            className="min-w-full flex justify-center items-center min-h-50"
-          >
-            <p className="text-center px-4">{announcement.message}</p>
-          </div>
-        ))}
+    <>
+      <div className="relative w-full py-2 md:px-6 overflow-hidden text-white bg-primary bg-opacity-90 rounded-sm border shadow-default border-stroke mb-5 p-5 flex justify-between dark:border-strokedark dark:bg-boxdark dark:text-white">
+        <div
+          className={`flex transition-transform duration-500 ease-in-out ${
+            !isTransitioning && 'transition-none'
+          }`}
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          onTransitionEnd={() => {
+            if (currentIndex === announcements.length) {
+              setIsTransitioning(false);
+              setCurrentIndex(0);
+            }
+          }}
+        >
+          {announcements.concat(announcements[0]).map((announcement, index) => (
+            <div
+              key={index}
+              className="min-w-full flex justify-center items-center min-h-50"
+            >
+              <p className="text-center px-4">{announcement.message}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,9 +1,5 @@
 import { Link } from 'react-router-dom';
-
-interface BreadcrumbProps {
-  pageName: string;
-  role: 'admin' | 'dealer';
-}
+import { BreadcrumbProps } from '../../types/package';
 
 const Breadcrumb = ({ pageName, role }: BreadcrumbProps) => {
   const basePath = {
@@ -14,22 +10,24 @@ const Breadcrumb = ({ pageName, role }: BreadcrumbProps) => {
   const { path, label } = basePath[role];
 
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
-        {pageName}
-      </h2>
+    <>
+      <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+          {pageName}
+        </h2>
 
-      <nav>
-        <ol className="flex items-center gap-2">
-          <li>
-            <Link className="font-medium" to={path}>
-              {label} /
-            </Link>
-          </li>
-          <li className="font-medium text-primary">{pageName}</li>
-        </ol>
-      </nav>
-    </div>
+        <nav>
+          <ol className="flex items-center gap-2">
+            <li>
+              <Link className="font-medium" to={path}>
+                {label} /
+              </Link>
+            </li>
+            <li className="font-medium text-primary">{pageName}</li>
+          </ol>
+        </nav>
+      </div>
+    </>
   );
 };
 
